648f83d519a4c434d7fc5ba5c6c23a14a57e6c26
Alex Ng <Karzza08@hotmail.com>
2014-07-01 16:59:22 +0800

    emeals

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'site#index'
+
+  get 'privacy' => 'sites#privacy'
+
 end
+
--- spec/features/visit_site_spec.rb
+++ spec/features/visit_site_spec.rb
@@ -5,4 +5,14 @@ feature 'visit the site' do
     visit '/'
     expect(page).to have_content_'Hello World!'
   end
+
+  scenario 'Privacy' do
+    visit '/privacy' do
+    expect(page.to have_content 'Privacy')
+  end
+
+  scenario 'Terms' do
+    visit '/Terms'
+    expect(page).to have_content 'Terms'
+  end
 end
